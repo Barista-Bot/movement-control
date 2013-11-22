1f15dea4c40881ac410623635fde9a5744140c0d
qgodefroi <qgodefroi@gmail.com>
2013-11-22 12:15:17 +0000

    Added priority system (untested)

--- scripts/movement_filter.py
+++ scripts/movement_filter.py
@@ -10,16 +10,30 @@ class MovementFilter():
     def __init__(self):
         rospy.init_node('movement_filter')
         
-        rospy.Subscriber('js_cmd_vel', Twist, self.cmdVelCallback)
+        rospy.Subscriber('xt_cmd_vel', Twist, self.xtionCallback)
+        rospy.Subscriber('js_cmd_vel', Twist, self.joystickCallback)
         rospy.Subscriber('stop_movement', Bool, self.stopCallback)
         self.cmd_pub = rospy.Publisher('cmd_vel', Twist)
+        self.cmdVel = Twist()
+        self.jsCmdVel = Twist()
+        self.xtCmdVel = Twist()
         self.stopAllMovement = 0
 
-    def cmdVelCallback(self, data):
+    def publishCmdVel(self):
         if not self.stopAllMovement:
-            self.cmd_pub.publish(data)
+            if not (self.jsCmdVel.linear.x == 0 and self.jsCmdVel.angular.z == 0):
+                self.cmdVel = self.jsCmdVel
+            else:
+                self.cmdVel = self.xtCmdVel
+            self.cmd_pub.publish(self.cmdVel)
         else:
             rospy.logerr("STOPPING ALL MOVEMENT")
+    
+    def joystickCallback(self, data):
+        self.jsCmdVel = data
+
+    def xtionCallback(self, data):
+        self.xtCmdVel = data
 
     def stopCallback(self, stop):
         self.stopAllMovement = stop
@@ -29,6 +43,7 @@ class MovementFilter():
         r = rospy.Rate(rate)
         try:
             while not rospy.is_shutdown():
+                self.publishCmdVel()
                 r.sleep()
         except rospy.ROSInterruptException:
             pass
