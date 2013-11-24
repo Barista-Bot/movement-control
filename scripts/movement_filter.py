4fa1ae56ce4ac52b96f8cf7566317809ca4b6c06
qgodefroi <qgodefroi@gmail.com>
2013-11-24 16:11:00 +0000

    Updated movement_filter to check for front and back stop commands

--- scripts/movement_filter.py
+++ scripts/movement_filter.py
@@ -2,7 +2,7 @@
 
 import rospy
 
-from std_msgs.msg import Bool
+from movement.msg import Stop
 from geometry_msgs.msg import Twist
 
 class MovementFilter(): 
@@ -12,32 +12,44 @@ class MovementFilter():
         
         rospy.Subscriber('xt_cmd_vel', Twist, self.xtionCallback)
         rospy.Subscriber('js_cmd_vel', Twist, self.joystickCallback)
-        rospy.Subscriber('stop_movement', Bool, self.stopCallback)
+        rospy.Subscriber('stop_movement', Stop, self.stopCallback)
         self.cmd_pub = rospy.Publisher('cmd_vel', Twist)
         self.cmdVel = Twist()
         self.jsCmdVel = Twist()
         self.xtCmdVel = Twist()
-        self.stopAllMovement = 0
+        self.stopMovement = []
+        self.stopMovement.append(False)
+        self.stopMovement.append(False)
 
     def publishCmdVel(self):
-        if not self.stopAllMovement:
-            if abs(self.jsCmdVel.linear.x)>0 or abs(self.jsCmdVel.angular.z)>0:
-                self.cmdVel = self.jsCmdVel
-            else:
-                self.cmdVel = self.xtCmdVel
+        if abs(self.jsCmdVel.linear.x)>0 or abs(self.jsCmdVel.angular.z)>0:
+            self.cmdVel = self.jsCmdVel
+        else:
+            self.cmdVel = self.xtCmdVel
+            self.cmd_pub.publish(self.cmdVel)
+        
+        if self.cmdVel.linear.x == 0 and self.cmdVel.angular.z == 0:
+            self.cmd_pub.publish(self.cmdVel)
+        elif self.cmdVel.linear.x < 0 and not self.stopMovement[1]:
+            rospy.loginfo('Publishing backward move command')
+            self.cmd_pub.publish(self.cmdVel)
+
+        elif (self.cmdVel.linear.x > 0 or abs(self.cmdVel.angular.z) > 0) and not self.stopMovement[0]:
+            rospy.loginfo('Publishing forward move command')
             self.cmd_pub.publish(self.cmdVel)
         else:
-            rospy.logerr("STOPPING ALL MOVEMENT")
+            rospy.logwarn("Movement is not allowed, staying in position")
     
     def joystickCallback(self, data):
         self.jsCmdVel = data
-        rospy.loginfo('JOYSTICK ' + str(self.jsCmdVel))
 
     def xtionCallback(self, data):
         self.xtCmdVel = data
 
     def stopCallback(self, stop):
-        self.stopAllMovement = stop
+        self.stopMovement[0] = stop.stopFront
+        self.stopMovement[1] = stop.stopBack
+        rospy.loginfo('Front stop bool updated to ' + str(self.stopMovement[0]) + ' - Back stop bool updated to ' + str(self.stopMovement[1]))
 
     def runFilter(self, rate=10):
         rospy.loginfo("Starting movement filter")
